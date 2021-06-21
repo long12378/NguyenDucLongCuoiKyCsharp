77f1e6f82fb96764301a818b8a9660b5213ce52b
long13278 <nduclong7@gmail.com>
2021-06-21 22:26:53 +0700

    Hello

---
+++ CuoiKyCSharp/Controllers/MainController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelEF.DAO;
+namespace CuoiKyCSharp.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            var model = new ProductDao().listpro();
+            return View(model);
+        }
+    }
+}
